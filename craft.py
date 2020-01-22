3fde345f05eb51359652a60d2ff956ccafe8f81a
Lucie Duhackova <lucieduhackova@gmail.com>
2020-01-22 20:17:25 +0100

    zmena ve vetvi json_csv, soubor craft.py

--- craft.py
+++ craft.py
@@ -1,6 +1,6 @@
 # craft.py
---- tady se budou delat zmeny pro vetev json_csv ---
+# --- tady se budou delat zmeny pro vetev json_csv ---
 # json_csv
 
 
----- konec pro json_csv ----------------------------
+# ---- konec pro json_csv ----------------------------
