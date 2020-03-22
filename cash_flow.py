8bd33c6ee332488a4853f3c8426f4afd1093ca71
Barbora Taborska <barbora.taborska@gmail.com>
2020-03-22 21:49:45 +0100

    Dotaz na zůstek - změna "s" na "z"

--- cash_flow.py
+++ cash_flow.py
@@ -1,7 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
-rozhodovaci_dotaz = input("Chceš vkládat (v), mazat (m) nebo aktualizovat (a), znat sumu prijmu (s) ")
+rozhodovaci_dotaz = input("Chceš vkládat (v), mazat (m) nebo aktualizovat (a), znat zůstatek (z) ")
 
 if rozhodovaci_dotaz == "v":
     datum = input("Napiš datum ve formátu: RRRR-MM-DD ")
@@ -19,7 +19,7 @@ elif rozhodovaci_dotaz == "a":
     vydaj_obleceni = input("Pokud za obleční napiš částku, pokud ne,napiš 0 ")
     vydaj_zabava = input("Pokud za zábavu napiš částku, pokud ne,napiš 0 ")
     vydaj_fix = input("Pokud za fix napiš částku, pokud ne,napiš 0 ")
-elif rozhodovaci_dotaz == "s":
+elif rozhodovaci_dotaz == "z":
     pass
 else:
     print("Nerozumím")
@@ -148,7 +148,7 @@ def main():
     elif rozhodovaci_dotaz == "a":
         update_radku = (prijem, vydaj_jidlo, vydaj_obleceni, vydaj_zabava, vydaj_fix, datum)
         update_row(conn, update_radku)
-    elif rozhodovaci_dotaz == "s":
+    elif rozhodovaci_dotaz == "z":
         closing_balance(conn)
 
 
